ebee021e4e4d7dca2feee525d695f4ff56d207bf
Sam <samlab@list.ru>
2026-06-04 00:09:32 +0300

    Update mopedmap.py

--- mopedmap.py
+++ mopedmap.py
@@ -526,7 +526,7 @@ def classify_post(text):
         return "interception"
     elif "авиацион" in text_lower and "бпла" not in text_lower and "беспилот" not in text_lower:
         return "aviation"
-    elif "меры безопасности" in text_lower or "пуск" in text_lower or "опасность" in text_lower or "угроз" in text_lower:
+    elif "меры безопасности" in text_lower or "пуск" in text_lower or "опасность" in text_lower or ("угроз" in text_lower and "в случае" not in text_lower):
         return "danger"
     elif "фиксаци" in text_lower or "пролёт" in text_lower or "группа" in text_lower:
         return "sighting"
