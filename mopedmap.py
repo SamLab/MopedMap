586757d55738ec7f94b8950f90dd8585ef5e4f0e
Sam <samlab@list.ru>
2026-06-29 11:41:46 +0300

    replace layer control with visible checkbox on map; fix legend onAdd pattern

--- mopedmap.py
+++ mopedmap.py
@@ -3775,7 +3775,7 @@ const lightningTileUrl = 'https://images.lightningmaps.org/blitzortung/europe/in
 const lightningOpts = {{ opacity: 0.9, maxZoom: 19, pane: 'lightning' }};
 const lightningLayer0 = L.tileLayer(lightningTileUrl + '0', Object.assign({{ attribution: 'Молнии: <a href=\"https://www.blitzortung.org\">Blitzortung.org</a>' }}, lightningOpts));
 const lightningLayer1 = L.tileLayer(lightningTileUrl + '1', Object.assign({{}}, lightningOpts));
-const lightningGroup = L.layerGroup([lightningLayer0, lightningLayer1]);
+const lightningGroup = L.layerGroup([lightningLayer0, lightningLayer1]).addTo(map);
 
 setInterval(function() {{
   const ts = '&_t=' + Date.now();
@@ -3826,7 +3826,20 @@ L.geoJSON(regionGeoJSON, {{
   }}
 }}).addTo(map);
 
-L.control.layers(null, {{ '⛈ Молнии': lightningGroup }}, {{ position: 'topleft', collapsed: true }}).addTo(map);
+const lightningCtrl = L.control({{ position: 'topleft' }});
+lightningCtrl.onAdd = function() {{
+  const div = L.DomUtil.create('div', '');
+  div.style.cssText = 'background:rgba(255,255,255,.9);padding:4px 8px;border-radius:6px;font-size:13px;border:1px solid #ccc;cursor:pointer;white-space:nowrap';
+  div.innerHTML = '<label style="cursor:pointer;color:#22c55e;font-weight:bold"><input type="checkbox" id="lightning-cb" checked> ⛈ Молнии (30 мин)</label>';
+  L.DomEvent.disableClickPropagation(div);
+  const cb = div.querySelector('#lightning-cb');
+  cb.addEventListener('change', function() {{
+    if (this.checked) lightningGroup.addTo(map);
+    else map.removeLayer(lightningGroup);
+  }});
+  return div;
+}};
+lightningCtrl.addTo(map);
 
 data.forEach(item => {{
   const s = styleMap[item.type] || styleMap.info;
@@ -3919,7 +3932,8 @@ if (useItem) {{
   document.getElementById('dist-info').textContent = `ближайшая опасность: ${{distKm}} км (${{useItem.name}}${{ago}})`;
 }}
 
-L.control({{ position: 'bottomright' }}).onAdd = function() {{
+const legendCtrl = L.control({{ position: 'bottomright' }});
+legendCtrl.onAdd = function() {{
   const div = L.DomUtil.create('div', 'legend');
   div.innerHTML = '<b>Легенда</b><br>' +
     '<i style="background:#e94560"></i> Опасность БПЛА<br>' +
@@ -3933,7 +3947,8 @@ L.control({{ position: 'bottomright' }}).onAdd = function() {{
     '<span style="display:inline-block;width:12px;height:12px;clip-path:polygon(50% 0%,61% 35%,98% 35%,68% 57%,79% 91%,50% 70%,21% 91%,32% 57%,2% 35%,39% 35%);background:#333;border:2px solid #00f5ff;vertical-align:middle;margin-right:6px"></span> Ярославль и область' +
     '<br><span style="font-size:11px;color:#888">Заливка = область в опасности</span>';
   return div;
-}}.addTo(map);
+}};
+legendCtrl.addTo(map);
 </script>
 </body>
 </html>"""
